2eaf03d3f9ea78ba807c500904f19bd9dbf6982c
Toby7658 <orlaghtobin@gmail.com>
2022-08-26 13:34:37 +0000

    Add style to background

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,82 +1,60 @@
-const canvas = document.getElementById("canvas");
+const cards = document.querySelectorAll(".card");
+let matched = 0;
+let cardOne, cardTwo;
+let disableDeck = false;
 
-/** list of cards to be displayed */
-let cardGame = () => [{
-        name: "bunny",
-        img: "assets/img/bunny.png"
-    },
-    {
-        name: "bunny",
-        img: "assets/img/bunny.png"
-    },
-    {
-        name: "purple",
-        img: "assets/img/purple.png"
-    },
-    {
-        name: "purple",
-        img: "assets/img/purple.png"
-    },
-    {
-        name: "fox",
-        img: "assets/img/fox.png"
-    },
-    {
-        name: "fox",
-        img: "assets/img/fox.png"
-    },
-    {
-        name: "grey",
-        img: "assets/img/grey.png"
-    },
-    {
-        name: "grey",
-        img: "assets/img/grey.png"
-    },
-    {
-        name: "sonic",
-        img: "assets/img/sonic.png"
-    },
-    {
-        name: "sonic",
-        img: "assets/img/sonic.png"
-    },
-    {
-        name: "red",
-        img: "assets/img/red.png"
-    },
-    {
-        name: "red",
-        img: "assets/img/red.png"
-    },
-    {
-        name: "eggman",
-        img: "assets/img/eggman.png"
-    },
-    {
-        name: "eggman",
-        img: "assets/img/eggman.png"
-    },
-    {
-        name: "yellow",
-        img: "assets/img/yellow.png"
-    },
-    {
-        name: "yellow",
-        img: "assets/img/yellow.png"
-    },
-];
-
-/** comment here */
-let randomize = () => {
-    let cardDisplay = cardGame();
-    //randomize the array from animalCards variable: //
-    cardDisplay.sort(() => Math.random() - 0.5);
-    return cardDisplay;
-};
-
-randomize();
-
-/** canvas */
-let gameboard = () => {
-   let cardDisplay = randomize();}
+function flipCard({target: clickedCard}) {
+    if(cardOne !== clickedCard && !disableDeck) {
+        clickedCard.classList.add("flip");
+        if(!cardOne) {
+            return cardOne = clickedCard;
+        }
+        cardTwo = clickedCard;
+        disableDeck = true;
+        let cardOneImg = cardOne.querySelector(".back-view img").src,
+        cardTwoImg = cardTwo.querySelector(".back-view img").src;
+        matchCards(cardOneImg, cardTwoImg);
+    }
+}
+function matchCards(img1, img2) {
+    if(img1 === img2) {
+        matched++;
+        if(matched == 8) {
+            setTimeout(() => {
+                return shuffleCard();
+            }, 1000);
+        }
+        cardOne.removeEventListener("click", flipCard);
+        cardTwo.removeEventListener("click", flipCard);
+        cardOne = cardTwo = "";
+        return disableDeck = false;
+    }
+    setTimeout(() => {
+        cardOne.classList.add("shake");
+        cardTwo.classList.add("shake");
+    }, 400);
+    setTimeout(() => {
+        cardOne.classList.remove("shake", "flip");
+        cardTwo.classList.remove("shake", "flip");
+        cardOne = cardTwo = "";
+        disableDeck = false;
+    }, 1200);
+}
+function shuffleCard() {
+    matched = 0;
+    disableDeck = false;
+    cardOne = cardTwo = "";
+    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
+    arr.sort(() => Math.random() > 0.5 ? 1 : -1);
+    cards.forEach((card, i) => {
+        card.classList.remove("flip");
+        let imgTag = card.querySelector(".back-view img");
+        imgTag.src = `images/img-${arr[i]}.png`;
+        card.addEventListener("click", flipCard);
+    });
+}
+shuffleCard();
+    
+cards.forEach(card => {
+    card.addEventListener("click", flipCard);
+});
