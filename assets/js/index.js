9291e28f346533ac82e6234a7612b43609b9daf4
Toby7658 <orlaghtobin@gmail.com>
2022-08-26 12:41:17 +0000

    Add initial structure to html and css

---
+++ assets/js/index.js
@@ -0,0 +1,69 @@
+const canvas = document.getElementById("canvas");
+
+/** ARRAY FOR GAMEBOARD LAYOUT */
+let cardGame = () => [{
+        name: "bunny",
+        img: "assets/img/bunny.png"
+    },
+    {
+        name: "bunny",
+        img: "assets/img/bunny.png"
+    },
+    {
+        name: "purple",
+        img: "assets/img/purple.png"
+    },
+    {
+        name: "purple",
+        img: "assets/img/purple.png"
+    },
+    {
+        name: "fox",
+        img: "assets/img/fox.png"
+    },
+    {
+        name: "fox",
+        img: "assets/img/fox.png"
+    },
+    {
+        name: "grey",
+        img: "assets/img/grey.png"
+    },
+    {
+        name: "grey",
+        img: "assets/img/grey.png"
+    },
+    {
+        name: "sonic",
+        img: "assets/img/sonic.png"
+    },
+    {
+        name: "sonic",
+        img: "assets/img/sonic.png"
+    },
+    {
+        name: "red",
+        img: "assets/img/red.png"
+    },
+    {
+        name: "red",
+        img: "assets/img/red.png"
+    },
+    {
+        name: "eggman",
+        img: "assets/img/eggman.png"
+    },
+    {
+        name: "eggman",
+        img: "assets/img/eggman.png"
+    },
+    {
+        name: "yellow",
+        img: "assets/img/yellow.png"
+    },
+    {
+        name: "yellow",
+        img: "assets/img/yellow.png"
+    },
+];
+
